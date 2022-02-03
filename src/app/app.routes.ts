71f0253f735042c7dacb1ab28ffe172afdbebfa8
sergio4540 <sergio4540@hotmail.com>
2022-02-03 11:40:12 -0500

    feat: heroes app

---
+++ src/app/app.routes.ts
@@ -0,0 +1,23 @@
+import { BuscadorComponent } from './components/buscador/buscador.component';
+import { HomeComponent } from './components/home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+
+
+
+const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'heroes', component: HeroesComponent },
+    { path: 'heroe/:id', component: HeroeComponent },
+    { path: 'buscar/:termino', component: BuscadorComponent },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
